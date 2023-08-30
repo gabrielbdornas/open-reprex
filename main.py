d7915b9ad14cb65d8b624fe16ca3e4f7930749b5
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-08-30 09:34:56 -0300

    Show not working process headless and it lang

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--lang=it')
 chrome_options.add_argument('--ignore-certificate-errors')
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
 chrome_options.add_argument(f'user-agent={user_agent}')
