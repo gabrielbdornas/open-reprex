4b6207f370bb2df03d2a11123ede8b5ea1f7814c
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-08-30 09:09:16 -0300

    Show not working process headless and lots options
    See https://stackoverflow.com/a/56074474/11755155

--- main.py
+++ main.py
@@ -15,7 +15,16 @@ search_text = "Stante l'elevata richiesta i posti disponibili per il servizio sc
 
 # Set up Chrome options for headless mode
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--proxy-server='direct://'")
+chrome_options.add_argument("--proxy-bypass-list=*")
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--ignore-certificate-errors')
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
 chrome_options.add_argument(f'user-agent={user_agent}')
 
