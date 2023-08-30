a612d9ce62b8f70dee04ca8fe3c064901d6ae53f
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-08-30 09:05:21 -0300

    Show not working process headless and win agent
    See https://stackoverflow.com/a/69129053/11755155

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ search_text = "Stante l'elevata richiesta i posti disponibili per il servizio sc
 # Set up Chrome options for headless mode
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
 chrome_options.add_argument(f'user-agent={user_agent}')
 
 
