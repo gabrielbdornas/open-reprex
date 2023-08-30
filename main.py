a53f216993d44b12954bafa24aa2bfa77471d845
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-08-30 12:09:56 -0300

    Show working after stackoverflow answer
    See https://stackoverflow.com/a/77008950/11755155

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from dotenv import load_dotenv
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from undetected_chromedriver import ChromeOptions
+import undetected_chromedriver as uc
 import time
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 
 load_dotenv()
 
@@ -17,24 +15,12 @@ password = os.getenv('PASSWORD')
 search_text = "Stante l'elevata richiesta i posti disponibili per il servizio scelto sono esauriti."
 
 # Set up Chrome options for headless mode
-chrome_options = Options()
-chrome_options.add_argument("--window-size=1920,1080")
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--proxy-server='direct://'")
-chrome_options.add_argument("--proxy-bypass-list=*")
-chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--lang=it')
-chrome_options.add_argument('--ignore-certificate-errors')
-user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
-chrome_options.add_argument(f'user-agent={user_agent}')
-
+chrome_options = ChromeOptions()
+chrome_options.add_argument("--headless")
 
 # Initialize the Selenium webdriver with the configured options
-driver = webdriver.Chrome(options=chrome_options)
+# driver = webdriver.Chrome(options=chrome_options)
+driver = uc.Chrome(headless=True,use_subprocess=False)
 driver.implicitly_wait(3)
 
 # Open the login page
@@ -42,17 +28,13 @@ driver.get(login_url)
 driver.get_screenshot_as_file("1_login_page.png")
 
 # Find the login and password input fields and fill them
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'Email'))).send_keys(login)
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME,'Password'))).send_keys(password)
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'g-recaptcha'))).click()
-
-# login_field = driver.find_element('name', 'Email')
-# password_field = driver.find_element('name', 'Password')
-# login_field.send_keys(login)
-# password_field.send_keys(password)
+login_field = driver.find_element('name', 'Email')
+password_field = driver.find_element('name', 'Password')
+login_field.send_keys(login)
+password_field.send_keys(password)
 
 # Submit the form (you can replace this with actual form submission method if needed)
-# password_field.send_keys(Keys.RETURN)
+password_field.send_keys(Keys.RETURN)
 
 # Allow some time for the page to load and the login to complete
 time.sleep(3)
